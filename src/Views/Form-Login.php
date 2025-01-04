4bac5281d49104075cc00836f0184a253b9efae3
George Castro <gamcastro@bol.com.br>
2025-01-04 18:48:39 -0300

    Commit Inicial

---
+++ src/Views/Form-Login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/assets/css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <title>Login</title>
+
+
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+
+        <section class="intro">
+            <h1>SGI</h1>
+            <p>Sistema de Gerenciamento Integrado</p>
+        </section>
+
+
+        <section class="formulario">
+
+
+            <h2>Acesso</h2>
+            <br>
+            <br>
+            <form action="" method="post">
+
+                <div class="formulario__input">
+                    <label for="usuario">Usuário:</label>
+                    <input type="text" id="usuario" name="usuario" placeholder="Digite aqui seu cpf">
+                    <br>
+                    <label for="password">Senha:</label>
+                    <div class="formulario__input__password">
+
+                        <input type="password" name="password" id="password" placeholder="Digite aqui sua senha">
+                        <button type="button" id="togglePassword">
+                            <i id="icon" class="fa fa-eye"></i>
+                        </button>
+                    </div>
+                    <a href="" class="formulario__input__esqueceu">Esqueçeu a senha?</a>
+                    <button>Login</button>
+                </div>
+            </form>
+
+
+            </div>
+            <!-- JavaScript -->
+            <script>
+                // Seleciona os elementos
+                const passwordInput = document.getElementById('password');
+                const togglePasswordButton = document.getElementById('togglePassword');
+                const icon = document.getElementById('icon');
+
+                // Adiciona o evento de clique no botão
+                togglePasswordButton.addEventListener('click', () => {
+                    // Alterna o tipo de input
+                    const isPassword = passwordInput.type === 'password';
+                    passwordInput.type = isPassword ? 'text' : 'password';
+
+                    // Alterna o ícone
+                    icon.className = isPassword ? 'fa fa-eye-slash' : 'fa fa-eye';
+                });
+            </script>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
